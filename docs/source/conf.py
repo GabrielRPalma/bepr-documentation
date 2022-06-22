dff404700689f2e4d755a147930ca321e4329776
Gabriel Rodrigues Palma <58814900+GabrielRPalma@users.noreply.github.com>
2022-06-22 16:59:35 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'bepr'
+copyright = '2022, Gabriel Rodrigues Palma'
+author = 'Gabriel'
 
 release = '0.1'
-version = '0.1.0'
+version = '0.0.2'
 
 # -- General configuration
 
